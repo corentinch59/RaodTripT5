f7ee886647b37076cb929edd7d4bec7dee4c3c6f
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2023-09-07 00:03:01 +0200

    Added AudioManager
    -Added Mixer
    -Added a test clip

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] _sounds;
+    [SerializeField] private AudioMixer _audioMixer;
+
+    private static AudioManager _instance;
+    public static AudioManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Debug.LogWarning("Duplicate AudioManager with name : " + name);
+            Destroy(this);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        foreach (Sound sound in _sounds)
+        {
+            SetSound(sound);
+        }
+    }
+
+    private void SetSound(Sound sound)
+    {
+        sound.Source = this.gameObject.AddComponent<AudioSource>();
+
+        sound.Source.clip = sound.Clip;
+        sound.Source.outputAudioMixerGroup = sound.Output;
+        sound.Source.loop = sound.Loop;
+        sound.Source.volume = sound.Volume;
+        sound.Source.pitch = sound.Pitch;
+    }
+
+    public void SetMasterVolume(float masterVolume)
+    {
+        _audioMixer.SetFloat("MasterVolume", masterVolume);
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound tempSound = Array.Find(_sounds, sound => sound.Name == name);
+        if (tempSound != null)
+        {
+            tempSound.Source.Play();
+        }
+        else
+        {
+            Debug.Log(tempSound.Name + "doesn't exist");
+        }
+        
+    }
+
+    public void PlaySound(AudioClip clip, AudioSource source)
+    {
+        source.clip = clip;
+        source.Play();
+    }
+}
