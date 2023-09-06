eb6f0d148d5075b65560779b0d37df1b99200e26
Haiito92 <hannaantoinebusiness@gmail.com>
2023-09-06 15:50:56 +0200

    Added DustCloud Event

---
+++ Assets/Scripts/Events/DustCloud.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustCloud : IEvent
+{
+    public void BeginEvent()
+    {
+        Debug.Log("Dust Began");
+        UIManager.Instance.Dusts.SetActive(true);
+    }
+
+    public void CompleteEvent()
+    {
+        Debug.Log("Dust Completed");
+        UIManager.Instance.Dusts.SetActive(false);
+    }
+
+    public void FailedEvent()
+    {
+        throw new System.NotImplementedException();
+    }
+}
