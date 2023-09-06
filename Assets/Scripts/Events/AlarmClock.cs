121f70131892702b3aa5bf947efc9dd3073fb199
Haiito92 <hannaantoinebusiness@gmail.com>
2023-09-06 17:17:38 +0200

    Added TrashEvent and AlarmClock Event

---
+++ Assets/Scripts/Events/AlarmClock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmClock : IEvent
+{
+    public void BeginEvent()
+    {
+        Debug.Log("Alarm Clock Began");
+    }
+
+    public void CompleteEvent()
+    {
+        Debug.Log("Alarm Clock Complete");
+    }
+
+    public void FailedEvent()
+    {
+        throw new System.NotImplementedException();
+    }
+}
