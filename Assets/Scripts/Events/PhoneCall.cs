aeb5f67faf33609f98014561148e04e1c1655ecd
Haiito92 <hannaantoinebusiness@gmail.com>
2023-09-07 11:52:25 +0200

    Cleaning the scripts

--- Assets/Scripts/Events/PhoneCall.cs
+++ Assets/Scripts/Events/PhoneCall.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PhoneCall : IEvent
@@ -14,7 +12,6 @@ public class PhoneCall : IEvent
     {
         Debug.Log("Phone Call Completed");
         UIManager.Instance.PhoneCallPopup.SetActive(false);
-        GameManager.Instance.BetweenEvents();
     }
 
     public void FailedEvent()
